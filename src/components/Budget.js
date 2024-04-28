645579f307d24ae61182db9a520735761ca412bd
csasia <theia@thiea-camilosasia>
2024-04-28 19:11:10 -0400

    Add Budget component

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,1 +1,20 @@
+import { useContext } from "react";
+import { AppContext } from "../context/AppContext";
 
+export default function Budget() {
+    const { budget, dispatch } = useContext(AppContext);
+
+    function handlebBudgetChange(event) {
+        dispatch({
+            type: "SET_BUDGET",
+            payload: event.target.value,
+        });
+    }
+
+    return (
+        <div className="alert alert-secondary">
+            <span style={{ marginRight: 20 }}>Budget: £</span>
+            <input type="number" step={10} value={budget} onChange={handlebBudgetChange} />
+        </div>
+    )
+}
