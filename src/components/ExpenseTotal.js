7e7becb8197f36a051a73e96304e17a4981914a3
csasia <theia@thiea-camilosasia>
2024-04-28 20:19:22 -0400

    Add ExpenseTotal component

--- src/components/ExpenseTotal.js
+++ src/components/ExpenseTotal.js
@@ -1,1 +1,14 @@
+import { useContext } from "react";
+import { AppContext } from "../context/AppContext";
 
+export default function ExpenseTotal() {
+    const { expenses } = useContext(AppContext);
+
+    const totalExpenses = expenses.reduce((total, { cost }) => total + cost, 0);
+
+    return (
+        <div className='alert alert-primary'>
+            <span>Spent so far: £{totalExpenses}</span>
+        </div>
+    );
+}
