e13daa7323ab26e97a957ab43fa8bb893de87af6
csasia <theia@thiea-camilosasia>
2024-04-28 22:35:03 -0400

    Add ExpenseList component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Budget from './components/Budget';
 import Remaining from './components/Remaining';
 import ExpenseTotal from './components/ExpenseTotal';
+import ExpenseList from './components/ExpenseList';
 
 import { AppProvider } from './context/AppContext';
 
@@ -14,37 +15,31 @@ const App = () => {
             <div className='container'>
                 <h1 className='mt-3'>Company's Budget Allocation</h1>
                 <div className='row mt-3'>
-                    {
-                        <div className='col-sm'>
-                            <Budget />
-                        </div>
-                    }
-
-                    {
-                        <div className='col-sm'>
-                            <Remaining />
-                        </div>
-                    }
-
-                    {
-                        <div className='col-sm'>
-                            <ExpenseTotal />
-                        </div>
-                    }
-
-                    {
-                        /* Add ExpenseList component here */
-                    }
-
-                    {
-                        /* Add ExpenseItem component here */
-                    }
-
-                    {
-                        /* Add AllocationForm component here under */
-                    }
-
+                    <div className='col-sm'>
+                        <Budget />
+                    </div>
+                    <div className='col-sm'>
+                        <Remaining />
+                    </div>
+                    <div className='col-sm'>
+                        <ExpenseTotal />
+                    </div>
                 </div>
+
+                <h3 className='mt-3'>Allocation</h3>
+                <div className='row'>
+                    <div className='col-sm'>
+                        <ExpenseList />
+                    </div>
+                </div>
+
+                {
+                    /* Add ExpenseItem component here */
+                }
+
+                {
+                    /* Add AllocationForm component here under */
+                }
             </div>
         </AppProvider>
     );
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,1 +1,34 @@
+import { useContext } from 'react';
+import { TiDelete } from 'react-icons/ti';
+import { AppContext } from '../context/AppContext';
 
+export default function ExpenseItem(props) {
+    const { dispatch } = useContext(AppContext);
+
+    function increaseAllocation(name) {
+        dispatch({
+            type: "ADD_EXPENSE",
+            payload: {
+                name,
+                cost: 10,
+            }
+        })
+
+    }
+
+    function handleDeleteExpense() {
+        dispatch({
+            type: "DELETE_EXPENSE",
+            payload: props.id,
+        })
+    }
+
+    return (
+        <tr>
+            <td>{props.name}</td>
+            <td>£{props.cost}</td>
+            <td><button onClick={() => increaseAllocation(props.name)}>+</button></td>
+            <td><TiDelete size='1.5em' onClick={handleDeleteExpense}></TiDelete></td>
+        </tr>
+    );
+}
