7a4191357e77e08962e6b157d0fdacc4ca0402b7
csasia <csasia@email.com>
2024-04-30 15:29:41 -0400

    Show current currency elsewhere

--- src/components/ExpenseTotal.js
+++ src/components/ExpenseTotal.js
@@ -2,13 +2,13 @@ import { useContext } from "react";
 import { AppContext } from "../context/AppContext";
 
 export default function ExpenseTotal() {
-    const { expenses } = useContext(AppContext);
+    const { expenses, currency } = useContext(AppContext);
 
     const totalExpenses = expenses.reduce((total, { cost }) => total + cost, 0);
 
     return (
         <div className='alert alert-primary'>
-            <span>Spent so far: £{totalExpenses}</span>
+            <span>Spent so far: {currency}{totalExpenses}</span>
         </div>
     );
 }
--- src/components/Remaining.js
+++ src/components/Remaining.js
@@ -2,14 +2,14 @@ import { useContext } from "react";
 import { AppContext } from "../context/AppContext";
 
 export default function Remaining() {
-    const { budget, expenses } = useContext(AppContext);
+    const { budget, expenses, currency } = useContext(AppContext);
 
     const totalExpenses = expenses.reduce((total, { cost }) => total + cost, 0);
     const alertType = totalExpenses > budget ? "alert-danger" : "alert-success";
 
     return (
         <div className={`alert ${alertType}`}>
-            <span>Remaining: £{budget - totalExpenses}</span>
+            <span>Remaining: {currency}{budget - totalExpenses}</span>
         </div>
     );
 }
